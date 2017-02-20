28b3ceb7cefb274dc9d678a93ee1c64275bc5046
virre <virre_olsson@hotmail.com>
2017-02-20 12:42:22 +0100

    created loop in html for modal

--- src/pages/home/mesure-modal.ts
+++ src/pages/home/mesure-modal.ts
@@ -2,19 +2,40 @@ import { Component } from '@angular/core';
 import {NavParams} from 'ionic-angular';
 
 @Component({
+  selector: 'mesure-modal',
   templateUrl: 'mesure-modal.html'
 })
 export class MesureModal {
 
 color:string;
 measures:string;
+measureList = [];
 
   constructor(private navParams: NavParams){
     this.color = this.navParams.get("color");
     this.measures = this.navParams.get("measure");
 
-  }
-  printMeasures(){
-    
+    if (this.measures == "normal"){
+      this.measureList[0] = {measure:"measure 3"};
+      this.measureList[1] = {measure:"measure 4"};
+    }
+
+    else if (this.measures == "abnormal"){
+      this.measureList[0] = {measure:"measure 1"};
+      this.measureList[1] = {measure:"measure 2"};
+    }
+
+    else if (this.measures == "fatal"){
+      this.measureList[0] = {measure:"measure 5"};
+      this.measureList[1] = {measure:"measure 6"};
+    }
+
+    else {
+      let measureList = [
+        { measure: 'Jilles'},
+        { measure: 'Todd'},
+        { measure: 'Lisa'}
+      ];
+    }
   }
 }
