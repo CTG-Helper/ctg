fd5324e68356abeecdf0a34213b8b67f66379f7a
Martin <Martin@macbooks-MacBook-Pro.local>
2017-03-14 12:17:48 +0100

    About us

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -7,6 +7,17 @@ import {TranslateService, LangChangeEvent} from "ng2-translate";
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  aboutHeading:string;
+  aboutContentOne:string;
+  aboutContentTwo:string;
+  aboutContentThree:string;
+  aboutContentFour:string;
+  aboutTeam: string;
+  aboutOwner: string;
+  aboutOwnerText: string;
+  aboutDeveloper: string;
+  aboutContact: string;
+  aboutItalic: string;
 
   constructor(private navParams: NavParams,
               private viewCtrl: ViewController,
@@ -17,6 +28,18 @@ export class AboutPage {
   ngOnInit() {
     this.viewCtrl.setBackButtonText(this.translateService.instant('BACK'));
     this.initText();
+    this.aboutHeading = this.translateService.instant("ABOUT_HEADING");
+    this.aboutContentOne = this.translateService.instant("ABOUT_CONTENT_1");
+    this.aboutContentTwo = this.translateService.instant("ABOUT_CONTENT_2");
+    this.aboutContentThree = this.translateService.instant("ABOUT_CONTENT_3");
+    this.aboutContentFour = this.translateService.instant("ABOUT_CONTENT_4");
+    this.aboutTeam = this.translateService.instant("ABOUT_TEAM");
+    this.aboutOwner = this.translateService.instant("ABOUT_OWNER");
+    this.aboutOwnerText = this.translateService.instant("ABOUT_OWNER_TEXT");
+    this.aboutDeveloper = this.translateService.instant("ABOUT_DEVELOPER");
+    this.aboutContact = this.translateService.instant("ABOUT_CONTACT");
+    this.aboutItalic = this.translateService.instant("ABOUT_ITALIC");
+
   }
 
 
