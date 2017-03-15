33829452ca743aa95bf3bd1932d56efce55727dc
Rasmus Josefsson <josefssonrasmus@hotmail.se>
2017-03-15 11:37:46 +0100

    removed unused imports

--- src/pages/start/start.ts
+++ src/pages/start/start.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
 import {HomePage} from "../home/home";
 import {AboutPage} from "../about/about";
 import {InstructionsPage} from "../instructions/instructions";
